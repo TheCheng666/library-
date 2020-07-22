4dc50b18a70b2508f3f9fde4ec66c555443cf1a1
程鑫博 <2906690344@qq.com>
2020-07-22 11:46:30 +0800

    第三次提交

--- src/main/java/com/interceptor/LoginInterceptor.java
+++ src/main/java/com/interceptor/LoginInterceptor.java
@@ -21,7 +21,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String uri = request.getRequestURI();
-        if (uri.indexOf("login") >= 0 || uri.indexOf("register") >= 0 || uri.indexOf("checkregister") >= 0 || uri.indexOf("Vcode") >= 0 || uri.indexOf("static") >= 0) {
+        if (uri.indexOf("login") >= 0 || uri.indexOf("register") >= 0 || uri.indexOf("checkregister") >= 0 || uri.indexOf("Vcode") >= 0 ) {
             if (request.getSession().getAttribute("LoginException") != null) {
                 request.getSession().removeAttribute("LoginException");
             }
@@ -29,34 +29,31 @@ public class LoginInterceptor implements HandlerInterceptor {
         } else {
             HttpSession session = request.getSession();
             Account account = (Account) session.getAttribute("accountinfo");
-            if(account!=null){
-                return true;
-            }else {
-                throw new LoginException("用户未登录！");
+            if (account != null) {
+                if (uri.indexOf("Manager") >= 0) {
+                    if (account.getRole() == 1) {
+                        return true;
+                    } else {
+                        throw new LoginException("用户身份错误!");
+                    }
+                }
+                else if (uri.indexOf("User") >= 0) {
+                    if (account.getRole() == 0) {
+                        return true;
+                    } else {
+                        throw new LoginException("用户身份错误！");
+                    }
+                }
+                else {
+                    return true;
+                }
+            } else {
+                throw new LoginException("用户未登录");
             }
-//            if (account != null) {
-//                if (uri.indexOf("Manager") >= 0) {
-//                    if (account.getRole() == 1) {
-//                        return true;
-//                    } else {
-//                        throw new LoginException("用户身份错误!");
-//                    }
-//                }
-//                if (uri.indexOf("User") >= 0) {
-//                    if (account.getRole() == 0) {
-//                        return true;
-//                    } else {
-//                        throw new LoginException("用户身份错误！");
-//                    }
-//                }
-//            } else {
-//                throw new LoginException("用户未登录");
-//            }
-//            return true;
         }
     }
 
-   //执行controller方法后，在ModelAndView对象之前，统一处理数据和试图
+    //执行controller方法后，在ModelAndView对象之前，统一处理数据和试图
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
